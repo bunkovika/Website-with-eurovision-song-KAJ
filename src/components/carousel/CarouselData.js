d72f90435484477356c1ab0171de8b23ee0aacac
Viktroiia Bunko <bunkovik@student.cvut.cz>
2024-04-02 21:37:58 +0200

    adding service worker

--- src/components/carousel/CarouselData.js
+++ src/components/carousel/CarouselData.js
@@ -4,7 +4,7 @@ const data = [
         id: 0,
         country: 'CZECH',
         image: '../images/Czech.svg',
-        audio: 'https://topmusicua.com/uploads/files/2024-02/1707238060_65c26297499f3404282495.mp3',
+        audio: 'https://p.scdn.co/mp3-preview/9a880b10fce03fcbb548495e00e92491b05cddd2?cid=ad5ebaf37746484ca61e02068fd2988e',
         song: 'PEDESTAL',
     },
     {
@@ -18,7 +18,7 @@ const data = [
         id:2,
         country: 'NORWAY',
         image: '../images/Norway.svg',
-        audio: 'https://topmusicua.com/uploads/files/2023-01/1673170113_d090d180d182d0b5d0bc20d09fd0b8d0b2d0bd0b2d0b0d180d0bd0b22020d09cd0b0d0bdd196d184d0b5d181d182.mp3',
+        audio: 'https://p.scdn.co/mp3-preview/a9371cbf54aabba2a3c4c5756920265333eb664b?cid=9c1e714b207b46429963ee8fa8bf0044',
         song: 'ULVEHAM',
     }
 ];
--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+if('serviceWorker' in navigator){
+    window.addEventListener('load', ()=>{
+        navigator.serviceWorker
+            .register('./sw_cached_pages.js')
+            .then(reg => console.log('Service Worker: Registered'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`))
+    })
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
