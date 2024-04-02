e40e172028e7915d8b766e8ede47ea70bdf07410
Viktroiia Bunko <bunkovik@student.cvut.cz>
2024-04-02 22:00:44 +0200

    changing path to images

--- src/components/carousel/CarouselData.js
+++ src/components/carousel/CarouselData.js
@@ -1,23 +1,26 @@
+import czech from "../../assets/images/Czech.svg";
+import ukraine from "../../assets/images/Ukraine.svg";
+import norway from "../../assets/images/Norway.svg";
 // all items from carousel
 const data = [
     {
         id: 0,
         country: 'CZECH',
-        image: '../images/Czech.svg',
+        image: czech,
         audio: 'https://p.scdn.co/mp3-preview/9a880b10fce03fcbb548495e00e92491b05cddd2?cid=ad5ebaf37746484ca61e02068fd2988e',
         song: 'PEDESTAL',
     },
     {
         id: 1,
         country: 'UKRAINE',
-        image: '../images/Ukraine.svg',
+        image: ukraine,
         audio: 'https://topmusicua.com/uploads/files/2024-02/1707041003_ln20ln20_20jrr20hl2020trs2020mr.mp3',
         song: 'MARIA&TEREZA',
     },
     {
         id:2,
         country: 'NORWAY',
-        image: '../images/Norway.svg',
+        image: norway,
         audio: 'https://p.scdn.co/mp3-preview/a9371cbf54aabba2a3c4c5756920265333eb664b?cid=9c1e714b207b46429963ee8fa8bf0044',
         song: 'ULVEHAM',
     }
